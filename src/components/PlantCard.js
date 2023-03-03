95582b7aa43a956ad1aa1d34ca91500b269cb21a
jonathan bola <bolajonathan@yahoo.com>
2023-03-03 13:59:56 -0600

    out of stock feature

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {useState}from "react";
 
 function PlantCard({plant}) {
+  const [isInStock, setIsInStock]= useState(true)
+  function handleClick(){
+   setIsInStock(!isInStock)
+  }
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isInStock ? (
+        <button className="primary" onClick={handleClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
     </li>
   );
